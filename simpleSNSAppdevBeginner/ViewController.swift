666642a3b427e2c266b9a711a8901981adb9893a
Yuta iwasaki <yanyong0127@gmail.com>
2021-06-25 12:11:09 +0900

    共有などで使う機能一覧画面を表示させる処理の実装

--- simpleSNSAppdevBeginner/ViewController.swift
+++ simpleSNSAppdevBeginner/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var imageView: UIImageView!
+    //toolbarのアクションが押された時の処理
+    @IBAction func showActivityView(_ sender: UIBarButtonItem) {
+        //↓ActivityViewControllerは共有などで使う機能一覧画面
+       //↓これだけだと表示されない
+        let controller = UIActivityViewController.init(activityItems: [imageView.image!], applicationActivities: nil)//インスタンス化
+        //モーダルビュー（controllerがモーダルビューの対象）viewの上に共有などで使う機能一覧画面を貼り付けるイメージ　completionはクロージャで完了した後に何かしたい時に使う
+       self.present(controller, animated: true, completion: nil)
     }
-
-
 }
 
